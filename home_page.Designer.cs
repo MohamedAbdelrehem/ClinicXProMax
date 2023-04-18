4e0b74e18fce71407422b14835342fe28ef41173
Mohamed Abdelrehem <mohamedabdelrehem32@gmail.com>
2023-04-18 16:41:50 +0200

    intialize home page

--- home_page.Designer.cs
+++ home_page.Designer.cs
@@ -29,40 +29,24 @@ namespace Clinic_Mang_Sys
         /// </summary>
         private void InitializeComponent()
         {
-            this.adminButton = new System.Windows.Forms.Button();
-            this.loginButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
-            // adminButton
+            // panel1
             // 
-            this.adminButton.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.adminButton.Location = new System.Drawing.Point(344, 162);
-            this.adminButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.adminButton.Name = "adminButton";
-            this.adminButton.Size = new System.Drawing.Size(176, 65);
-            this.adminButton.TabIndex = 0;
-            this.adminButton.Text = "admin";
-            this.adminButton.UseVisualStyleBackColor = false;
-            // 
-            // loginButton
-            // 
-            this.loginButton.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.loginButton.Location = new System.Drawing.Point(87, 162);
-            this.loginButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.loginButton.Name = "loginButton";
-            this.loginButton.Size = new System.Drawing.Size(160, 65);
-            this.loginButton.TabIndex = 1;
-            this.loginButton.Text = "login";
-            this.loginButton.UseVisualStyleBackColor = false;
-            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 366);
+            this.panel1.TabIndex = 0;
             // 
             // home_page
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(686, 366);
-            this.Controls.Add(this.loginButton);
-            this.Controls.Add(this.adminButton);
+            this.Controls.Add(this.panel1);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "home_page";
             this.Text = "Home";
@@ -72,7 +56,6 @@ namespace Clinic_Mang_Sys
 
         #endregion
 
-        private System.Windows.Forms.Button adminButton;
-        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Panel panel1;
     }
 }
