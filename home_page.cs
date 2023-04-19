a52cffb874aa8e7d3de5c2fc31e3cbbd29bcbde0
Mohamed Abdelrehem <mohamedabdelrehem32@gmail.com>
2023-04-20 00:26:11 +0200

    fix conflict 2

--- home_page.cs
+++ home_page.cs
@@ -8,17 +8,45 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Clinic_Mang_Sys {
-public partial class home_page : Form {
-  public home_page() { InitializeComponent(); }
+namespace Clinic_Mang_Sys
+{
+    public partial class home_page : Form
+    {
+        public home_page() { InitializeComponent(); }
 
-  private void loginButton_Click(object sender, EventArgs e) {}
+        private void buttonPatiants_Click(object sender, EventArgs e) { }
 
-  private void button1_Click(object sender, EventArgs e) {}
+        private void panelSideMenu_Paint(object sender, PaintEventArgs e) { }
 
-  private void buttonPatiants_Click(object sender, EventArgs e) {}
+        private void button_Dashboard_Click(object sender, EventArgs e)
+        {
 
-  private void panelSideMenu_Paint(object sender, PaintEventArgs e) {}
-}
+        }
+
+        private void button_Appoinment_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Setting_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Clinic_Setting_Subsetting_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Clinic_Program_Subsetting_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_About_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
 
 }
