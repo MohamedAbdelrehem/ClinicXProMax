b4a9a04fa6abeedb08f2d52b0359f6eff6ae2d2d
Restyled.io <commits@restyled.io>
2023-04-19 23:28:26 +0000

    Restyled by clang-format

--- Program.cs
+++ Program.cs
@@ -6,14 +6,14 @@ using System.Windows.Forms;
 
 namespace Clinic_Mang_Sys {
 static class Program {
-    /// <summary>
-    /// The main entry point for the application.
-    /// </summary>
-    [STAThread]
-    static void Main() {
-        Application.EnableVisualStyles();
-        Application.SetCompatibleTextRenderingDefault(false);
-        Application.Run(new FrmMain());
-    }
+  /// <summary>
+  /// The main entry point for the application.
+  /// </summary>
+  [STAThread]
+  static void Main() {
+    Application.EnableVisualStyles();
+    Application.SetCompatibleTextRenderingDefault(false);
+    Application.Run(new FrmMain());
+  }
 }
 }
