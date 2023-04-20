f99eef7e7ce8215a3e54a748676ad62974efad35
Restyled.io <commits@restyled.io>
2023-04-20 03:11:01 +0000

    Restyled by clang-format

--- Pages/PageAppointments.Designer.cs
+++ Pages/PageAppointments.Designer.cs
@@ -1,41 +1,41 @@
 ﻿
 namespace Clinic_Mang_Sys.Pages {
 partial class PageAppointments {
-    /// <summary>
-    /// Required designer variable.
-    /// </summary>
-    private System.ComponentModel.IContainer components = null;
+  /// <summary>
+  /// Required designer variable.
+  /// </summary>
+  private System.ComponentModel.IContainer components = null;
 
-    /// <summary>
-    /// Clean up any resources being used.
-    /// </summary>
-    /// <param name="disposing">true if managed resources should be disposed;
-    /// otherwise, false.</param>
-    protected override void Dispose(bool disposing) {
-        if (disposing && (components != null)) {
-            components.Dispose();
-        }
-        base.Dispose(disposing);
+  /// <summary>
+  /// Clean up any resources being used.
+  /// </summary>
+  /// <param name="disposing">true if managed resources should be disposed;
+  /// otherwise, false.</param>
+  protected override void Dispose(bool disposing) {
+    if (disposing && (components != null)) {
+      components.Dispose();
     }
+    base.Dispose(disposing);
+  }
 
-    #region Component Designer generated code
+#region Component Designer generated code
 
-    /// <summary>
-    /// Required method for Designer support - do not modify
-    /// the contents of this method with the code editor.
-    /// </summary>
-    private void InitializeComponent() {
-        this.SuspendLayout();
-        //
-        // PageAppointments
-        //
-        this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.Name = "PageAppointments";
-        this.Load += new System.EventHandler(this.PageAppointments_Load);
-        this.ResumeLayout(false);
-    }
+  /// <summary>
+  /// Required method for Designer support - do not modify
+  /// the contents of this method with the code editor.
+  /// </summary>
+  private void InitializeComponent() {
+    this.SuspendLayout();
+    //
+    // PageAppointments
+    //
+    this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+    this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+    this.Name = "PageAppointments";
+    this.Load += new System.EventHandler(this.PageAppointments_Load);
+    this.ResumeLayout(false);
+  }
 
-    #endregion
+#endregion
 }
 }
