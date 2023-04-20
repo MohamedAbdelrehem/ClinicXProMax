749774a771472e798e9486696d022295d436029e
Restyled.io <commits@restyled.io>
2023-04-20 02:41:37 +0000

    Restyled by clang-format

--- Pages/PageAppointments.cs
+++ Pages/PageAppointments.cs
@@ -8,19 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Clinic_Mang_Sys.Pages
-{
-public partial class PageAppointments : UserControl
-{
-    public PageAppointments()
-    {
-        //if (this.IsInDesignMode()) return;
-        InitializeComponent();
-    }
+namespace Clinic_Mang_Sys.Pages {
+public partial class PageAppointments : UserControl {
+  public PageAppointments() {
+    // if (this.IsInDesignMode()) return;
+    InitializeComponent();
+  }
 
-    private void PageAppointments_Load(object sender, EventArgs e)
-    {
-
-    }
+  private void PageAppointments_Load(object sender, EventArgs e) {}
 }
 }
--- Program.cs
+++ Program.cs
@@ -5,36 +5,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Clinic_Mang_Sys
-{
-internal static class Program
-{
-    /// <summary>
-    /// The main entry point for the application.
-    /// </summary>
-    [STAThread]
-    private static void Main()
-    {
-        Application.EnableVisualStyles();
-        Application.SetCompatibleTextRenderingDefault(false);
-        Application.Run(new FrmMain());
-    }
+namespace Clinic_Mang_Sys {
+internal static class Program {
+  /// <summary>
+  /// The main entry point for the application.
+  /// </summary>
+  [STAThread]
+  private static void Main() {
+    Application.EnableVisualStyles();
+    Application.SetCompatibleTextRenderingDefault(false);
+    Application.Run(new FrmMain());
+  }
 
-    public static bool IsInDesignMode(this UserControl container)
-    {
-        if (Application.ExecutablePath.IndexOf("devenv.exe", StringComparison.OrdinalIgnoreCase) > -1)
-        {
-            container.Controls.Add(new Label()
-            {
-                Text = container.GetType().Name,
-                AutoSize = false,
-                TextAlign = ContentAlignment.MiddleCenter,
-                Dock = DockStyle.Fill
-            });
+  public static bool IsInDesignMode(this UserControl container) {
+    if (Application.ExecutablePath.IndexOf(
+            "devenv.exe", StringComparison.OrdinalIgnoreCase) > -1) {
+      container.Controls.Add(
+          new Label() { Text = container.GetType().Name, AutoSize = false,
+                        TextAlign = ContentAlignment.MiddleCenter,
+                        Dock = DockStyle.Fill });
 
-            return true;
-        }
-        return false;
+      return true;
     }
+    return false;
+  }
 }
 }
