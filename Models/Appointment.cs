407b308c4f3f408e8882284d6a18d6771d04a5f1
Restyled.io <commits@restyled.io>
2023-04-20 18:55:42 +0000

    Restyled by astyle

--- Models/Appointment.cs
+++ Models/Appointment.cs
@@ -7,22 +7,46 @@ using ServiceStack.DataAnnotations;
 
 namespace Clinic_Mang_Sys.Models
 {
-    //[AutoGenerateTable(2)]  from tool
-    [Alias("appointment")]
-    public class Appointment
-    {
-        [AutoIncrement, PrimaryKey]
-        public int Id { get; set; }
-        //[ForeignKey(typeof(Patient), OnDelete = "CASCADE" ) ] need Patient only
-        public string Name { get; set; }
-
-        public string Phone { get; set; } = "";
-        public string Address { get; set; } = "";
-        public string Email { get; set; } = "";
-
-        public DateTime DOB { get; set; } = DateTime.Now;
-
-        public string Gender { get; set; } = "";
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
+//[AutoGenerateTable(2)]  from tool
+[Alias("appointment")]
+public class Appointment
+{
+    [AutoIncrement, PrimaryKey]
+    public int Id {
+        get;
+        set;
     }
+    //[ForeignKey(typeof(Patient), OnDelete = "CASCADE" ) ] need Patient only
+    public string Name {
+        get;
+        set;
+    }
+
+    public string Phone {
+        get;
+        set;
+    } = "";
+    public string Address {
+        get;
+        set;
+    } = "";
+    public string Email {
+        get;
+        set;
+    } = "";
+
+    public DateTime DOB {
+        get;
+        set;
+    } = DateTime.Now;
+
+    public string Gender {
+        get;
+        set;
+    } = "";
+    public DateTime CreatedAt {
+        get;
+        set;
+    } = DateTime.Now;
+}
 }
