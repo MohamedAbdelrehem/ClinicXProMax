317a248e2cf351928cdc979e3745b8832748f0e1
Mohamed Abdelrehem <mohamedabdelrehem32@gmail.com>
2023-05-04 19:10:18 +0300

    add lang and theme files for future features

---
+++ Lib/Lang.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic_Mang_Sys.Lib
+{
+    class Lang
+    {
+        public static string lang;
+    }
+}
---
+++ Lib/ThemeColor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic_Mang_Sys.Lib
+{
+    class ThemeColor
+    {
+        public static Color grayColor = Color.FromArgb(32, 30, 45);
+        public static Color orangeColor = ColorTranslator.FromHtml("#FFBD2C");
+        public static Color pinkColor = ColorTranslator.FromHtml("#E06488");
+        public static Color cheryColor = ColorTranslator.FromHtml("#E23D4D");
+        public static Color currentColor = grayColor;
+    }
+}
