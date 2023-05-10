a61708ede34daf5c7f086ecce2099e07c0f34e5e
Mohamed Abdelrehem <mohamedabdelrehem32@gmail.com>
2023-05-10 03:38:18 +0300

    updating libraries

--- Lib/Common.cs
+++ Lib/Common.cs
@@ -1,15 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Kimtoo.BindingProvider;
 
 namespace Clinic_Mang_Sys.Lib
 {
     public static class Common
     {
-        public static DataGridViewAvatar GetAvatar(string Name) =>
-            new DataGridViewAvatar { Value = Name, };
+
+        public static Image ToImage(this byte[] byteArrayIn)
+        {
+            MemoryStream ms = new MemoryStream(byteArrayIn, 0, byteArrayIn.Length);
+            ms.Position = 0;
+            return Image.FromStream(ms, true);
+        }
+
+        public static byte[] ToBytes(this Image imageIn)
+        {
+
+            using (var ms = new MemoryStream())
+            {
+                imageIn.Save(ms, imageIn.RawFormat);
+                return ms.ToArray();
+
+            }
+
+        }
     }
 }
